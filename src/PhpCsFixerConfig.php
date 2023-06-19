05e715947cd5cfdaa2473a5e4da1922896209af6
Bruno Gaspar <brunofgaspar1@gmail.com>
2023-06-19 08:45:16 +0100

    chore: Swap deprecated rules
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/PhpCsFixerConfig.php
+++ src/PhpCsFixerConfig.php
@@ -195,10 +195,9 @@ class PhpCsFixerConfig extends Config
          * Namespace Notation
          */
         'blank_line_after_namespace' => true,
+        'blank_lines_before_namespace' => false,
         'clean_namespace' => true,
-        'no_blank_lines_before_namespace' => false,
         'no_leading_namespace_whitespace' => true,
-        'single_blank_line_before_namespace' => true,
 
         /**
          * Operator
